f10630d3660b5e836b73ecc67915269a19815005
Mario Souto <soutomarios@gmail.com>
2021-03-30 18:45:59 -0300

    add theme transition

--- packages/ui/src/theme/GlobalStyle/index.ts
+++ packages/ui/src/theme/GlobalStyle/index.ts
@@ -2,12 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     * {
-        box-sizing: border-box;
+      box-sizing: border-box;
+      transition: color .2s, background-color .2s;
     } 
     body {
-        font-family: sans-serif;
-        background-color: ${({ theme }) => theme.colors.background.main.color};
-        color: ${({ theme }) => theme.colors.background.main.contrastColor};
+      font-family: sans-serif;
+      background-color: ${({ theme }) => theme.colors.background.main.color};
+      color: ${({ theme }) => theme.colors.background.main.contrastColor};
     }
 `;
 
