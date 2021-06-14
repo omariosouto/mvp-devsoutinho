9217c0c43f1ca5d77664618f6ab393412c12f36c
Mario Souto <soutomarios@gmail.com>
2021-06-13 22:33:08 -0300

    add contributions sync with stars page

--- packages/site/cms/modules/youtube/index.ts
+++ packages/site/cms/modules/youtube/index.ts
@@ -3,35 +3,73 @@ import xmlParser from 'fast-xml-parser';
 import { IS_PROD } from '../../infra/db/createConfig';
 
 async function cacheVideos(videos: Array<YouTubeVideo>) {
-  // const cacheResult = videos.map(async (video) => {
-
-  //   console.log('result',result);
-
-  //   return true;
-  // });
-  // console.log('oi:cacheVideos:after');
-
   const result = videos.map(async (video) => {
-    const contribution = await fetch('http://localhost:3000/api/graphql', {
+    const query = `mutation {
+      createContribution(input: {
+        lang: PT_BR,
+        title: "${video.title}",
+        date: "${video.date}",
+        url: "${video.link}",
+        description: "${video.description}",
+      }) {
+        _id
+      }
+    }`;
+    const cacheResult = await fetch('http://localhost:3000/api/graphql/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(`
-        mutation {
-          createContribution(input: {
-            lang: PT_BR,
-            title: ${video.title},
-            date: ${video.date},
-            url: ${video.link},
-            description: ${video.description},
-          }) {
-            _id
-          }
+      body: JSON.stringify({
+        query,
+      }),
+    })
+      .then(async (response) => {
+        const responseData = await response.json();
+        const alreadyExists = responseData.errors;
+        if (alreadyExists) {
+          return true;
         }
-      `),
-    });
-    return contribution;
+
+        return false;
+      })
+      .then(async (isInCache) => {
+        // console.log('isInCache', isInCache, video.title);
+        if (!isInCache) {
+          console.log('Heey, GH API Call!!!');
+          await fetch('https://github-stars-api.herokuapp.com/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: process.env.GH_STARS_TOKEN,
+            },
+            body: JSON.stringify({
+              query: `
+                mutation {
+                  createContributions(data:
+                    [{
+                      title:"${video.title}"
+                      url:"${video.link}"
+                      description:"${video.description}"
+                      type:VIDEO_PODCAST
+                      date: "${video.date}"
+                    }]
+                    )
+                    {
+                      id
+                    }
+                }
+              `,
+            }),
+          })
+            .then((res) => res.text())
+            .then((data) => console.log(data));
+        }
+
+        return isInCache;
+      });
+
+    return cacheResult;
   });
   await Promise.all(result);
 
@@ -48,16 +86,21 @@ const resolvers = {
         .then(async (res) => {
           const response = await res.text();
           const parsedResponse = xmlParser.parse(response);
-          return parsedResponse.feed.entry.map((video) => ({
-            id: video['yt:videoId'],
-            date: new Date(video.published).toISOString(),
-            link: `https://youtu.be/${video['yt:videoId']}`,
-            title: video.title,
-            description: video['media:group']['media:description'].split(
-              '\n'
-            )[0],
-            thumbnail: `http://i1.ytimg.com/vi/${video['yt:videoId']}/hqdefault.jpg`,
-          }));
+          return parsedResponse.feed.entry.map((video, index) => {
+            const description = video['media:group']['media:description']
+              .split('\n')[0]
+              .replace(/\\\\o/g, '')
+              .replace(/\\o/g, '');
+
+            return {
+              id: video['yt:videoId'],
+              date: new Date(video.published).toISOString(),
+              link: `https://youtu.be/${video['yt:videoId']}`,
+              title: video.title,
+              description,
+              thumbnail: `http://i1.ytimg.com/vi/${video['yt:videoId']}/hqdefault.jpg`,
+            };
+          });
         })
         .then(async (videos) => {
           if (!IS_PROD) {
